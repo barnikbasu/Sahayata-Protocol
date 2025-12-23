966b733966b2759c8b8dc9ecac2c4d9d72a40489
Barnik Basu <barnikbasu@gmail.com>
2025-12-23 09:37:14 +0530

    Update ReliefRegistry.sol

--- contracts/ReliefRegistry.sol
+++ contracts/ReliefRegistry.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ReliefRegistry is Ownable {
+    enum Category { NONE, FOOD, MEDICAL, SHELTER }
+    
+    mapping(address => bool) public isBeneficiary;
+    mapping(address => Category) public merchantType;
+
+    constructor() Ownable(msg.sender) {}
+
+    function registerBeneficiary(address _victim) external onlyOwner {
+        isBeneficiary[_victim] = true;
+    }
+
+    function registerMerchant(address _shop, uint8 _cat) external onlyOwner {
+        merchantType[_shop] = Category(_cat);
+    }
+}
